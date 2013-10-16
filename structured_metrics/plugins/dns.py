01c8ade90ffd4338292201fcb4a456db8870f044
Devin Bayer <dbayer@ripe.net>
2013-10-16 16:49:45 +0200

    add bind and fix load plugin

---
+++ structured_metrics/plugins/dns.py
@@ -0,0 +1,20 @@
+from . import Plugin
+
+
+class RipeDnslugin(Plugin):
+    """Collect metrics for name servers.
+
+    Works with diamond's bind collector
+    """
+    targets = [
+        {
+            'match': '^servers\.(?P<server>[^\.]+)\.bind\.view\.(?P<view>.+)\.(?P<category>[^\.]+)\.(?P<what>.*)$',
+            'target_type': 'counter',
+        },
+        {
+            'match': '^servers\.(?P<server>[^\.]+)\.bind\.(?P<category>[^\.]+)\.(?P<what>.*)$',
+            'target_type': 'counter',
+        },
+    ]
+
+# vim: ts=4 et sw=4:
