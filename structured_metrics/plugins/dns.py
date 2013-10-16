187e164f10f5a71816eab14539cabff686736fa4
Devin Bayer <dbayer@ripe.net>
2013-10-16 17:06:50 +0200

    better dns

--- structured_metrics/plugins/dns.py
+++ structured_metrics/plugins/dns.py
@@ -8,13 +8,29 @@ class RipeDnsPlugin(Plugin):
     """
     targets = [
         {
-            'match': '^servers\.(?P<server>[^\.]+)\.bind\.view\.(?P<view>.+)\.(?P<category>[^\.]+)\.(?P<what>.*)$',
+            'match': '^servers\.(?P<server>[^\.]+)\.bind\.view\.(?P<view>.+)\.(?P<category>[^\.]+)\.(?P<name>.*)$',
             'target_type': 'counter',
         },
         {
-            'match': '^servers\.(?P<server>[^\.]+)\.bind\.(?P<category>[^\.]+)\.(?P<what>.*)$',
+            'match': '^servers\.(?P<server>[^\.]+)\.bind\.(?P<category>[^\.]+)\.(?P<name>.*)$',
             'target_type': 'counter',
         },
     ]
+
+    def sanitize(self, target):
+        cat = target['tags']['category']
+        
+        if cat == 'sockstat':
+            what = 'connections'
+        elif cat == 'queries':
+            what = 'queries'
+        elif cat == 'nsstat':
+            what = 'queries'
+        elif cat == 'resstat':
+            what = 'results'
+        else:
+            what = 'unknown'
+
+        target['tags']['what'] = what
 
 # vim: ts=4 et sw=4:
